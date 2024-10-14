8d358b186bcbfff9c395f1fbbeda0dd85065a357
ninjiacoder <ninjiacoder@gmail.com>
2024-10-14 23:27:47 +0800

    feat: project init

---
+++ swift-follow/swift_followApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_followApp.swift
+//  swift-follow
+//
+//  Created by ZiyuanZhao on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct swift_followApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-followTests/swift_followTests.swift
@@ -0,0 +1,16 @@
+//
+//  swift_followTests.swift
+//  swift-followTests
+//
+//  Created by ZiyuanZhao on 2024/10/14.
+//
+
+import Testing
+
+struct swift_followTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
